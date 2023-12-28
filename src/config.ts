27d606ef9cb7dd9c3c6c27ffc589d9e279d5fba4
Utsav-2345 <105980720+Utsav-2345@users.noreply.github.com>
2023-12-29 03:42:17 +0530

    zone

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
   WalletAdapterNetwork.Mainnet) as WalletAdapterNetwork;
 // const network = WalletAdapterNetwork.Devnet;
-export const rpcHost = 'https://dimensional-quiet-moon.solana-mainnet.quiknode.pro/9e040159f3d3d1deae6e313e2a8a38d57c507e3f//';
+export const rpcHost = 'https://dimensional-quiet-moon.solana-mainnet.quiknode.pro/9e040159f3d3d1deae6e313e2a8a38d57c507e3f/';
 
 export const candyMachineId = new PublicKey(
     "2STv1Te4MZKR1ezooTnmoq1C1AMJYHegtz4SGzyEDfST"
